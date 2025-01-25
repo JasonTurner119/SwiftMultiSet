961b71d2aede7b60693c0754e4a42215c6a8ffcd
JasonTurner <jasonturner119@icloud.com>
2025-01-25 01:03:54 -0800

    Initial commit.

--- Sources/SwiftMultiSet/SwiftMultiSet.swift
+++ Sources/SwiftMultiSet/SwiftMultiSet.swift
@@ -1,2 +1,149 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+public struct MultiSet<Element: Hashable> {
+	
+	public private(set) var count: Int
+	public var isEmpty: Bool { count == 0 }
+	public var distintCount: Int { _counts.count }
+	
+	private var _counts: [Element: Int]
+	
+	public init() {
+		self.count = 0
+		self._counts = [:]
+	}
+	
+	public init(_ elements: some Sequence<Element>) {
+		self.init()
+		for element in elements {
+			self.insert(element)
+		}
+	}
+	
+	public mutating func insert(_ element: Element, count: Int = 1) {
+		precondition(count >= 0)
+		self[element] += count
+	}
+	
+	public mutating func remove(_ element: Element, count: Int = 1) {
+		precondition(self[element] >= count)
+		self[element] -= count
+	}
+	
+	public subscript(element: Element) -> Int {
+		_read {
+			yield self._counts[element, default: 0]
+		}
+		_modify {
+			let oldValue = self._counts[element, default: 0]
+			yield &self._counts[element, default: 0]
+			let newValue = self._counts[element, default: 0]
+			precondition(newValue >= 0)
+			self.count += (newValue - oldValue)
+			if self._counts[element] == 0 {
+				self._counts[element] = nil
+			}
+		}
+	}
+	
+}
+
+extension MultiSet: Sequence {
+	
+	public func makeIterator() -> Iterator {
+		return Iterator(self)
+	}
+	
+	public struct Iterator: IteratorProtocol {
+		
+		private var dictionaryIterator: [Element: Int].Iterator
+		private var current: (element: Element, remaining: Int)?
+		
+		fileprivate init(_ multiSet: MultiSet) {
+			self.dictionaryIterator = multiSet._counts.makeIterator()
+			self.current = nil
+		}
+		
+		public mutating func next() -> Element? {
+			if self.current == nil {
+				guard let next = self.dictionaryIterator.next() else { return nil }
+				self.current = (element: next.key, remaining: next.value)
+			}
+			assert(self.current != nil)
+			let result = self.current!.element
+			self.current!.remaining -= 1
+			if self.current!.remaining == 0 {
+				self.current = nil
+			}
+			return result
+		}
+		
+	}
+	
+}
+
+extension MultiSet: ExpressibleByArrayLiteral {
+	
+	public init(arrayLiteral elements: Element...) {
+		self.init(elements)
+	}
+	
+}
+
+extension MultiSet: Equatable {
+	
+	public static func == (lhs: MultiSet<Element>, rhs: MultiSet<Element>) -> Bool {
+		guard lhs.count == rhs.count else { return false }
+		guard lhs.distintCount == rhs.distintCount else { return false }
+		for element in lhs._counts.keys {
+			if lhs[element] != rhs[element] { return false }
+		}
+		return true
+	}
+	
+}
+
+extension MultiSet: Hashable { }
+
+extension MultiSet {
+	
+	public func union(_ other: MultiSet<Element>) -> MultiSet<Element> {
+		var union = self
+		for element in other._counts.keys {
+			union[element] = Swift.max(self[element], other[element])
+		}
+		return union
+	}
+	
+	public func intersection(_ other: MultiSet<Element>) -> MultiSet<Element> {
+		var intersection = MultiSet<Element>()
+		for element in other._counts.keys {
+			intersection[element] = Swift.min(self[element], other[element])
+		}
+		return intersection
+	}
+	
+	public mutating func formUnion(_ other: MultiSet<Element>) {
+		self = self.union(other)
+	}
+	
+	public mutating func formIntersection(_ other: MultiSet<Element>) {
+		self = self.intersection(other)
+	}
+	
+}
+
+extension MultiSet: CustomStringConvertible {
+	
+	public var description: String {
+		"MultiSet(\(_counts.description))"
+	}
+	
+}
+
+private extension Dictionary {
+	
+	func sorted<T: Comparable>(by mapping: (Element) -> T) -> [Element] {
+		return self.sorted(by: { mapping($0) < mapping($1) })
+	}
+	
+}
--- Tests/SwiftMultiSetTests/SwiftMultiSetTests.swift
+++ Tests/SwiftMultiSetTests/SwiftMultiSetTests.swift
@@ -1,6 +1,210 @@
 import Testing
 @testable import SwiftMultiSet
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+// MARK: Basic Operations
+
+@Test func emptySet() async throws {
+	let set: MultiSet<Int> = []
+	#expect(set.isEmpty)
+	#expect(set.count == 0)
+}
+
+@Test func insertionDeletion() async throws {
+	var set: MultiSet<Int> = []
+	#expect(set.count == 0 && set.distintCount == 0)
+	set.insert(23)
+	set.insert(100, count: 10)
+	#expect(set.count == 11 && set.distintCount == 2)
+	set.remove(100, count: 9)
+	set.insert(23, count: 2)
+	#expect(set.count == 4 && set.distintCount == 2)
+	set.remove(100)
+	set.remove(23)
+	set.remove(23, count: 2)
+	#expect(set.count == 0 && set.distintCount == 0)
+}
+
+@Test func subscripting() async throws {
+	var set: MultiSet<Int> = []
+	#expect(set.count == 0 && set.distintCount == 0)
+	set.insert(23)
+	#expect(set[23] == 1)
+	#expect(set[24] == 0)
+	set.insert(100, count: 10)
+	#expect(set[100] == 10)
+	set[100] = 0
+	set[23] += 10
+	#expect(set[100] == 0)
+	#expect(set[23] == 11)
+}
+
+// MARK: Equatable & Hashable
+
+@Test func equatableNoDuplicates() async throws {
+	let first: MultiSet = [1, 2, 3, 5, 9]
+	let second: MultiSet = [3, 1, 2, 5, 9]
+	#expect(first == second)
+	#expect(first.hashValue == second.hashValue)
+}
+
+@Test func equatableDuplicates() async throws {
+	let first: MultiSet = [1, 2, 2, 3, 3, 5, 9]
+	let second: MultiSet = [3, 2, 1, 2, 5, 3, 9]
+	#expect(first == second)
+	#expect(first.hashValue == second.hashValue)
+}
+
+@Test func equatableEmpty() async throws {
+	let first: MultiSet<Int> = []
+	let second: MultiSet<Int> = []
+	#expect(first == second)
+	#expect(first.hashValue == second.hashValue)
+}
+
+// MARK: Intersection
+
+@Test func emptySetIntersection() async throws {
+	let first: MultiSet<Int> = []
+	let second: MultiSet<Int> = []
+	let intersection = first.intersection(second)
+	#expect(intersection == [])
+	#expect(intersection.count == 0)
+	#expect(intersection.distintCount == 0)
+}
+
+@Test func noIntersection() async throws {
+	let first: MultiSet = [1, 2, 3, 4, 5]
+	let second: MultiSet = [6, 7, 8, 9, 10]
+	let intersection = first.intersection(second)
+	#expect(intersection == [])
+	#expect(intersection.count == 0)
+	#expect(intersection.distintCount == 0)
+}
+
+@Test func partialIntersection() async throws {
+	let first: MultiSet = [1, 2, 3, 3, 5, 9]
+	let second: MultiSet = [2, 1, 2, 3, 3]
+	let intersection = first.intersection(second)
+	#expect(intersection == [1, 2, 3, 3])
+	#expect(intersection.count == 4)
+	#expect(intersection.distintCount == 3)
+}
+
+@Test func totalIntersection() async throws {
+	let first: MultiSet = [1, 2, 2, 3, 3, 5, 9]
+	let second: MultiSet = [3, 2, 1, 2, 5, 3, 9]
+	let intersection = first.intersection(second)
+	#expect(intersection == [1, 2, 2, 3, 3, 5, 9])
+	#expect(intersection.count == 7)
+	#expect(intersection.distintCount == 5)
+}
+
+@Test func formEmptySetIntersection() async throws {
+	var first: MultiSet<Int> = []
+	let second: MultiSet<Int> = []
+	first.formIntersection(second)
+	#expect(first == [])
+	#expect(first.count == 0)
+	#expect(first.distintCount == 0)
+}
+
+@Test func formNoIntersection() async throws {
+	var first: MultiSet = [1, 2, 3, 4, 5]
+	let second: MultiSet = [6, 7, 8, 9, 10]
+	first.formIntersection(second)
+	#expect(first == [])
+	#expect(first.count == 0)
+	#expect(first.distintCount == 0)
+}
+
+@Test func formPartialIntersection() async throws {
+	var first: MultiSet = [1, 2, 3, 3, 5, 9]
+	let second: MultiSet = [2, 1, 2, 3, 3]
+	first.formIntersection(second)
+	#expect(first == [1, 2, 3, 3])
+	#expect(first.count == 4)
+	#expect(first.distintCount == 3)
+}
+
+@Test func formTotalIntersection() async throws {
+	var first: MultiSet = [1, 2, 2, 3, 3, 5, 9]
+	let second: MultiSet = [3, 2, 1, 2, 5, 3, 9]
+	first.formIntersection(second)
+	#expect(first == [1, 2, 2, 3, 3, 5, 9])
+	#expect(first.count == 7)
+	#expect(first.distintCount == 5)
+}
+
+// MARK: Union
+
+@Test func emptySetUnion() async throws {
+	let first: MultiSet<Int> = []
+	let second: MultiSet<Int> = []
+	let intersection = first.union(second)
+	#expect(intersection == [])
+	#expect(intersection.count == 0)
+	#expect(intersection.distintCount == 0)
+}
+
+@Test func nonOverlappingUnion() async throws {
+	let first: MultiSet = [1, 2, 3, 4, 5]
+	let second: MultiSet = [6, 7, 8, 9, 10]
+	let intersection = first.union(second)
+	#expect(intersection == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+	#expect(intersection.count == 10)
+	#expect(intersection.distintCount == 10)
+}
+
+@Test func partiallyOverlappingUnion() async throws {
+	let first: MultiSet = [1, 2, 3, 3, 5, 9]
+	let second: MultiSet = [2, 1, 2, 3, 3]
+	let intersection = first.union(second)
+	#expect(intersection == [1, 2, 2, 3, 3, 5, 9])
+	#expect(intersection.count == 7)
+	#expect(intersection.distintCount == 5)
+}
+
+@Test func totallyOverlappingUnion() async throws {
+	let first: MultiSet = [1, 2, 2, 3, 3, 5, 9]
+	let second: MultiSet = [3, 2, 1, 2, 5, 3, 9]
+	let intersection = first.union(second)
+	#expect(intersection == [1, 2, 2, 3, 3, 5, 9])
+	#expect(intersection.count == 7)
+	#expect(intersection.distintCount == 5)
+}
+
+@Test func formEmptySetUnion() async throws {
+	var first: MultiSet<Int> = []
+	let second: MultiSet<Int> = []
+	first.formUnion(second)
+	#expect(first == [])
+	#expect(first.count == 0)
+	#expect(first.distintCount == 0)
+}
+
+@Test func formNonOverlappingUnion() async throws {
+	var first: MultiSet = [1, 2, 3, 4, 5]
+	let second: MultiSet = [6, 7, 8, 9, 10]
+	first.formUnion(second)
+	#expect(first == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+	#expect(first.count == 10)
+	#expect(first.distintCount == 10)
+}
+
+@Test func formPartiallyOverlappingUnion() async throws {
+	var first: MultiSet = [1, 2, 3, 3, 5, 9]
+	let second: MultiSet = [2, 1, 2, 3, 3]
+	first.formUnion(second)
+	#expect(first == [1, 2, 2, 3, 3, 5, 9])
+	#expect(first.count == 7)
+	#expect(first.distintCount == 5)
+}
+
+@Test func formTotallyOverlappingUnion() async throws {
+	var first: MultiSet = [1, 2, 2, 3, 3, 5, 9]
+	let second: MultiSet = [3, 2, 1, 2, 5, 3, 9]
+	first.formUnion(second)
+	#expect(first == [1, 2, 2, 3, 3, 5, 9])
+	#expect(first.count == 7)
+	#expect(first.distintCount == 5)
 }
